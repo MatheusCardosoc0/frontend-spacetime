63d8b77bf4f29fd0b78e851580562ded1eb94fe1
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2023-05-21 19:21:41 -0300

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
 import EmptyMemories from '@/components/EmptyMemories'
-import Hero from '@/components/Hero'
-import Profile from '@/components/Profile'
-import SignIn from '@/components/SignIn'
-import { cookies } from 'next/headers'
 
 export default function Home() {
   return <EmptyMemories />
